c706dfc3c5196ac7a6680424cbaa9fc8befbb745
Jimkwan <jimkwan.yau@gmail.com>
2019-03-22 16:57:47 +0800

    [Update] Patch scramble core

--- patch/index.js
+++ patch/index.js
@@ -1,22 +1,27 @@
-import scramble_3x3 from './scramble/3x3'
+import Scrambo from 'scrambo'
 
-const handlerMap = {
-  // '2x2': ,
-  '3x3': scramble_3x3,
-  // '4x4': ,
-  // '5x5': ,
-  // '6x6': ,
-  // '7x7': ,
-  // CLK: ,
-  // MEG: ,
-  // PYR: ,
-  // SKB: ,
-  // SQ1: ,
-  '3x3-OH': scramble_3x3,
-  '3x3-WF': scramble_3x3,
-  // '3x3-BF': ,
-  // '4x4-BF': ,
-  // '5x5-BF': ,
+function getScramble(type, steps) {
+  var scramble = new Scrambo().type(type)
+  if (['444', '555', '666', '777', 'minx'].indexOf(type) !== -1) {
+    scramble = scramble.length(steps)
+  }
+  return scramble.get()
+}
+
+var handlerMap = {
+  '2x2': getScramble.bind(null, '222', 10),
+  '3x3': getScramble.bind(null, '333', 20),
+  '4x4': getScramble.bind(null, '444', 45),
+  '5x5': getScramble.bind(null, '555', 60),
+  '6x6': getScramble.bind(null, '666', 80),
+  '7x7': getScramble.bind(null, '777', 100),
+  '3x3-OH': getScramble.bind(null, '333', 25),
+  '3x3-WF': getScramble.bind(null, '333', 25),
+  'Square-1': getScramble.bind(null, 'sq1', 12),
+  'Pyraminx': getScramble.bind(null, 'pyram', 12),
+  'Skewb': getScramble.bind(null, 'skewb', 8),
+  'Megaminx': getScramble.bind(null, 'minx', 75),
+  'Clock': getScramble.bind(null, 'clock', 16)
 }
 
 exports.supportedTypes = Object.keys(handlerMap)
@@ -25,6 +30,6 @@ exports.generateScramble = function (type) {
   if (exports.supportedTypes.indexOf(type) === -1) {
     return 'Unsupported Cube Type'
   } else {
-    return handlerMap[type]()
+    return handlerMap[type]()[0]
   }
 }
--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,17 @@
+import { uglify } from 'rollup-plugin-uglify'
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+
 export default {
   input: 'patch/index.js',
   output: {
     name: 'qtimer',
     format: 'iife',
     file: 'patch/dist/qtimer.min.js'
-  }
+  },
+  plugins: [
+    resolve(),
+    commonjs(),
+    uglify()
+  ]
 }
